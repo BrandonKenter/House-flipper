bd46c28787ddd0f05440342eddf52b4f88b84101
Brandon Kenter <bjkenter@wisc.edu>
2020-12-05 19:17:48 -0600

    Create ButtonsAndLabels.java

---
+++ ButtonsAndLabels.java
@@ -0,0 +1,205 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.shape.Circle;
+
+public class ButtonsAndLabels {
+    @FXML
+    public Label rd0;
+    @FXML
+    public Label rd1;
+    @FXML
+    public Label rd2;
+    @FXML
+    public Label rd3;
+    @FXML
+    public Label rd4;
+    @FXML
+    public Label rd5;
+    @FXML
+    public Label rd6;
+    @FXML
+    public Label rd7;
+    @FXML
+    public Label rd8;
+    @FXML
+    public Label rd9;
+
+    @FXML
+    public Label mp0;
+    @FXML
+    public Label mp1;
+    @FXML
+    public Label mp2;
+    @FXML
+    public Label mp3;
+    @FXML
+    public Label mp4;
+    @FXML
+    public Label mp5;
+    @FXML
+    public Label mp6;
+    @FXML
+    public Label mp7;
+    @FXML
+    public Label mp8;
+    @FXML
+    public Label mp9;
+
+    @FXML
+    public Label ed0;
+    @FXML
+    public Label ed1;
+    @FXML
+    public Label ed2;
+    @FXML
+    public Label ed3;
+    @FXML
+    public Label ed4;
+    @FXML
+    public Label ed5;
+    @FXML
+    public Label ed6;
+    @FXML
+    public Label ed7;
+    @FXML
+    public Label ed8;
+    @FXML
+    public Label ed9;
+
+    @FXML
+    public CheckBox cb0;
+    @FXML
+    public CheckBox cb1;
+    @FXML
+    public CheckBox cb2;
+    @FXML
+    public CheckBox cb3;
+    @FXML
+    public CheckBox cb4;
+    @FXML
+    public CheckBox cb5;
+    @FXML
+    public CheckBox cb6;
+    @FXML
+    public CheckBox cb7;
+    @FXML
+    public CheckBox cb8;
+    @FXML
+    public CheckBox cb9;
+
+
+    @FXML
+    public Button cont;
+    @FXML
+    public Button end;
+
+
+
+    @FXML
+    public Button approve0;
+    @FXML
+    public Button discard0;
+    @FXML
+    public Circle circleStatus0;
+    @FXML
+    public Button prev0;
+    @FXML
+    public Button next0;
+    @FXML
+    public Label propertyLocation0;
+    @FXML
+    public Label propertySqFt0;
+    @FXML
+    public Label propertyPrice0;
+    @FXML
+    public Label propertyParagraph0;
+
+
+
+    @FXML
+    public Button approve1;
+    @FXML
+    public Button discard1;
+    @FXML
+    public Circle circleStatus1;
+    @FXML
+    public Button prev1;
+    @FXML
+    public Button next1;
+    @FXML
+    public Label propertyLocation1;
+    @FXML
+    public Label propertySqFt1;
+    @FXML
+    public Label propertyPrice1;
+    @FXML
+    public Label propertyParagraph1;
+
+
+
+    @FXML
+    public Button approve2;
+    @FXML
+    public Button discard2;
+    @FXML
+    public Circle circleStatus2;
+    @FXML
+    public Button prev2;
+    @FXML
+    public Button next2;
+    @FXML
+    public Label propertyLocation2;
+    @FXML
+    public Label propertySqFt2;
+    @FXML
+    public Label propertyPrice2;
+    @FXML
+    public Label propertyParagraph2;
+
+
+
+    @FXML
+    public Button approve3;
+    @FXML
+    public Button discard3;
+    @FXML
+    public Circle circleStatus3;
+    @FXML
+    public Button prev3;
+    @FXML
+    public Button next3;
+    @FXML
+    public Label propertyLocation3;
+    @FXML
+    public Label propertySqFt3;
+    @FXML
+    public Label propertyPrice3;
+    @FXML
+    public Label propertyParagraph3;
+
+
+
+    @FXML
+    public Button approve4;
+    @FXML
+    public Button discard4;
+    @FXML
+    public Circle circleStatus4;
+    @FXML
+    public Button prev4;
+    @FXML
+    public Button next4;
+    @FXML
+    public Label propertyLocation4;
+    @FXML
+    public Label propertySqFt4;
+    @FXML
+    public Label propertyPrice4;
+    @FXML
+    public Label propertyParagraph4;
+
+}
