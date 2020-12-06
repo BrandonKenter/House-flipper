d25065f47ced66a74c1917b846d87bdb53752be5
Brandon Kenter <bjkenter@wisc.edu>
2020-12-05 19:17:29 -0600

    Create Property.java

---
+++ Property.java
@@ -0,0 +1,23 @@
+package sample;
+
+public class Property {
+    String propertyLocation;
+    String propertySqFt;
+    String propertyPrice;
+    String propertyParagraph;
+    String rd0;
+    String mp0;
+    String ed0;
+
+    Property() {
+        this.propertyLocation = "Location - Madison, WI";
+        this.propertySqFt = "5000";
+        this.propertyPrice = "$555,555";
+        this.propertyParagraph = "EXAMPLE TEXT EXAMPLE TEXT EXAMPLE TEXT EXAMPLE TEXT EXAMPLE TEXT " +
+                "EXAMPLE TEXT EXAMPLE TEXT EXAMPLE TEXT EXAMPLE TEXT EXAMPLE TEXT EXAMPLE TEXT " +
+                "EXAMPLE TEXT EXAMPLE TEXT EXAMPLE TEXT EXAMPLE TEXT EXAMPLE TEXT EXAMPLE TEXT ";
+        this.rd0 = "NEW TEXT";
+        this.mp0 = "$55555";
+        this.ed0 = "99 hrs";
+    }
+}
