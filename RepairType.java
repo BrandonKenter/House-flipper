e408656f7d1213fc933c2e2f988b0420d2f5d473
Brandon Kenter <bjkenter@wisc.edu>
2020-12-18 12:58:43 -0600

    Update RepairType.java

--- RepairType.java
+++ RepairType.java
@@ -1,9 +1,15 @@
 package sample;
 
+/**
+ * The type of repair to be performed.
+ */
 public class RepairType {
-    String description;
-    String materialPrice;
-    String estimatedDuration;
+    String description; // Main description of what the repair is
+    String materialPrice; // Price of all materials to do the repair
+    String estimatedDuration; // Estimated duration of the repair
+    int valueAdded; // Percentage of added value to a property after this repair
+    int repairPriceTotal; // The total price calculated using materials and labor
+    Boolean checkButton = false; // Check button clicked status
 
     RepairType () {}
 }
