6757214fdf97f1923c0fd354c9999f36e166f2a1
Brandon Kenter <bjkenter@wisc.edu>
2020-12-18 12:59:04 -0600

    Update Property.java

--- Property.java
+++ Property.java
@@ -1,86 +1,31 @@
 package sample;
+import java.util.Random;
 
+/**
+ *
+ */
 public class Property {
-    String propertyLocation;
-    String propertySqFt;
-    String propertyPrice;
-    String propertyParagraph;
-    Boolean approved = null;
+    PropertyType propertyDetails;
 
-    String repairDescription0;
-    String repairDescription1;
-    String repairDescription2;
-    String repairDescription3;
-    String repairDescription4;
-    String repairDescription5;
-    String repairDescription6;
-    String repairDescription7;
-    String repairDescription8;
-    String repairDescription9;
-
-    String materialPrice0;
-    String materialPrice1;
-    String materialPrice2;
-    String materialPrice3;
-    String materialPrice4;
-    String materialPrice5;
-    String materialPrice6;
-    String materialPrice7;
-    String materialPrice8;
-    String materialPrice9;
-
-    String estimatedDuration0;
-    String estimatedDuration1;
-    String estimatedDuration2;
-    String estimatedDuration3;
-    String estimatedDuration4;
-    String estimatedDuration5;
-    String estimatedDuration6;
-    String estimatedDuration7;
-    String estimatedDuration8;
-    String estimatedDuration9;
-
-    Boolean checkButton0 = false;
-    Boolean checkButton1 = false;
-    Boolean checkButton2 = false;
-    Boolean checkButton3 = false;
-    Boolean checkButton4 = false;
-    Boolean checkButton5 = false;
-    Boolean checkButton6 = false;
-    Boolean checkButton7 = false;
-    Boolean checkButton8 = false;
-    Boolean checkButton9 = false;
-
+    /**
+     * Initialize a property's details and its list of repairs.
+     *
+     * @param page
+     */
     Property(int page) {
+        Random random = new Random();
         if (page == 0) {
+            // Retrieve random property from list and remove from list
+            int idx = random.nextInt(Properties.propertyTypes.size());
+            this.propertyDetails = Properties.propertyTypes.get(idx);
+            Properties.propertyTypes.remove(idx);
+
+            // Generate repair options for property
             for (int i = 0; i < 10; i++) {
                 // Assign property values
-
-            }
-        }
-        if (page == 1) {
-            for (int i = 0; i < 10; i++) {
-                // Assign property values
-
-            }
-        }
-        if (page == 2) {
-            for (int i = 0; i < 10; i++) {
-                // Assign property values
-
-            }
-        }
-
-        if (page == 3) {
-            for (int i = 0; i < 10; i++) {
-                // Assign property values
-
-            }
-        }
-        if (page == 4) {
-            for (int i = 0; i < 10; i++) {
-                // Assign property values
-
+                idx = random.nextInt(Repairs.repairTypes.size());
+                propertyDetails.propertyRepairs[i] = Repairs.repairTypes.get(idx);
+                Repairs.repairTypes.remove(idx);
             }
         }
     }
