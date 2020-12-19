280809ed5caa39020c8aef3666dee1aae0913b7a
Brandon Kenter <bjkenter@wisc.edu>
2020-12-18 23:43:41 -0600

    Added labor price attribute

--- RepairType.java
+++ RepairType.java
@@ -9,6 +9,7 @@ public class RepairType {
     String estimatedDuration; // Estimated duration of the repair
     int valueAdded; // Percentage of added value to a property after this repair
     int repairPriceTotal; // The total price calculated using materials and labor
+    String laborPrice; // The total price of labor to perform the repair
     Boolean checkButton = false; // Check button clicked status
 
     RepairType () {}
