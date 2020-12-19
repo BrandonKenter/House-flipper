89230b1c44112632ce97202213b0fc1968979f3f
Brandon Kenter <bjkenter@wisc.edu>
2020-12-19 08:07:57 -0600

    Update Property.java

--- Property.java
+++ Property.java
@@ -1,4 +1,5 @@
 package sample;
+import java.util.Arrays;
 import java.util.Random;
 
 /**
@@ -6,27 +7,30 @@ import java.util.Random;
  */
 public class Property {
     PropertyType propertyDetails;
+    int initialized = 0;
 
     /**
      * Initialize a property's details and its list of repairs.
-     *
-     * @param page
      */
-    Property(int page) {
-        Random random = new Random();
-        if (page == 0) {
-            // Retrieve random property from list and remove from list
-            int idx = random.nextInt(Properties.propertyTypes.size());
-            this.propertyDetails = Properties.propertyTypes.get(idx);
-            Properties.propertyTypes.remove(idx);
-
-            // Generate repair options for property
-            for (int i = 0; i < 10; i++) {
-                // Assign property values
-                idx = random.nextInt(Repairs.repairTypes.size());
-                propertyDetails.propertyRepairs[i] = Repairs.repairTypes.get(idx);
-                Repairs.repairTypes.remove(idx);
-            }
+    Property() {
+        if (initialized == 0) {
+            Properties properties = new Properties();
+            Repairs repairs = new Repairs();
         }
+
+        Random random = new Random();
+
+        // Retrieve random property from list and remove from list
+        int idx = random.nextInt(Properties.propertyTypes.size());
+        this.propertyDetails = Properties.propertyTypes.get(idx);
+        Properties.propertyTypes.remove(idx);
+
+        // Generate repair options for property
+        for (int i = 0; i < 10; i++) {
+            // Assign property values
+            idx = random.nextInt(Repairs.repairTypes.size());
+            this.propertyDetails.propertyRepairs[i] = Repairs.repairTypes.get(idx);
+            Repairs.repairTypes.remove(idx);
+            }
     }
 }
