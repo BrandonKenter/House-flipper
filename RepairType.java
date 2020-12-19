6dda6b5637fad8a2a52c9f684ce72614e3c1c2b5
Brandon Kenter <bjkenter@wisc.edu>
2020-12-19 00:27:34 -0600

    Updated naming conventions

--- RepairType.java
+++ RepairType.java
@@ -4,13 +4,14 @@ package sample;
  * The type of repair to be performed.
  */
 public class RepairType {
-    String description; // Main description of what the repair is
-    String materialPrice; // Price of all materials to do the repair
-    String estimatedDuration; // Estimated duration of the repair
-    int valueAdded; // Percentage of added value to a property after this repair
+    String repairDescription; // Main description of what the repair is
+    String repairMaterialPrice; // Price of all materials to do the repair
+    String repairEstimatedDuration; // Estimated duration of the repair
+    String repairValueAddedPercent; // Percentage of added value to a property after this repair
     int repairPriceTotal; // The total price calculated using materials and labor
-    String laborPrice; // The total price of labor to perform the repair
-    Boolean checkButton = false; // Check button clicked status
+    int repairValueAddedTotal; // The value added to the property with this repair
+    int repairLaborPrice; // The total price of labor to perform the repair
+    Boolean repairCheckButton = false; // Check button clicked status
 
     RepairType () {}
 }
