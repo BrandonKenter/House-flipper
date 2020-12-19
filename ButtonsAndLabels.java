3828626c6b59846bc6f10bb9222966b376407934
Brandon Kenter <bjkenter@wisc.edu>
2020-12-19 08:07:01 -0600

    Update ButtonsAndLabels.java

--- ButtonsAndLabels.java
+++ ButtonsAndLabels.java
@@ -1,226 +1,36 @@
 package sample;
-
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.shape.Circle;
+import java.util.Arrays;
+import java.util.Random;
 
 /**
- * Controls for the application.
+ *
  */
-public class ButtonsAndLabels {
-    /**
-     * First page.
-     */
-    @FXML
-    public Button cont;
+public class Property {
+    PropertyType propertyDetails;
+    int initialized = 0;
 
     /**
-     * Property pages.
+     * Initialize a property's details and its list of repairs.
      */
-    @FXML
-    public Label rd00;
-    @FXML
-    public Label rd01;
-    @FXML
-    public Label rd02;
-    @FXML
-    public Label rd03;
-    @FXML
-    public Label rd04;
-    @FXML
-    public Label rd05;
-    @FXML
-    public Label rd06;
-    @FXML
-    public Label rd07;
-    @FXML
-    public Label rd08;
-    @FXML
-    public Label rd09;
+    Property() {
+        if (initialized == 0) {
+            Properties properties = new Properties();
+            Repairs repairs = new Repairs();
+        }
 
-    @FXML
-    public Label mp00;
-    @FXML
-    public Label mp01;
-    @FXML
-    public Label mp02;
-    @FXML
-    public Label mp03;
-    @FXML
-    public Label mp04;
-    @FXML
-    public Label mp05;
-    @FXML
-    public Label mp06;
-    @FXML
-    public Label mp07;
-    @FXML
-    public Label mp08;
-    @FXML
-    public Label mp09;
+        Random random = new Random();
 
-    @FXML
-    public Label ed00;
-    @FXML
-    public Label ed01;
-    @FXML
-    public Label ed02;
-    @FXML
-    public Label ed03;
-    @FXML
-    public Label ed04;
-    @FXML
-    public Label ed05;
-    @FXML
-    public Label ed06;
-    @FXML
-    public Label ed07;
-    @FXML
-    public Label ed08;
-    @FXML
-    public Label ed09;
+        // Retrieve random property from list and remove from list
+        int idx = random.nextInt(Properties.propertyTypes.size());
+        this.propertyDetails = Properties.propertyTypes.get(idx);
+        Properties.propertyTypes.remove(idx);
 
-    @FXML
-    public CheckBox cb0;
-    @FXML
-    public CheckBox cb1;
-    @FXML
-    public CheckBox cb2;
-    @FXML
-    public CheckBox cb3;
-    @FXML
-    public CheckBox cb4;
-    @FXML
-    public CheckBox cb5;
-    @FXML
-    public CheckBox cb6;
-    @FXML
-    public CheckBox cb7;
-    @FXML
-    public CheckBox cb8;
-    @FXML
-    public CheckBox cb9;
-
-    @FXML
-    public Circle circleStatus0;
-    @FXML
-    public Circle circleStatus1;
-    @FXML
-    public Circle circleStatus2;
-    @FXML
-    public Circle circleStatus3;
-    @FXML
-    public Circle circleStatus4;
-
-    @FXML
-    public Button next0;
-    @FXML
-    public Button next1;
-    @FXML
-    public Button next2;
-    @FXML
-    public Button next3;
-    @FXML
-    public Button next4;
-
-    @FXML
-    public Label statusWarning0;
-    @FXML
-    public Button approve;
-    @FXML
-    public Button discard;
-    @FXML
-    public Circle circleStatus;
-    @FXML
-    public Button prev;
-    @FXML
-    public Button next;
-    @FXML
-    public Label propertyLocation;
-    @FXML
-    public Label propertySqFt;
-    @FXML
-    public Label propertyPrice;
-    @FXML
-    public Label propertyParagraph;
-
-
-    /**
-     * Report page
-     */
-    @FXML
-    public Label purchasePrice0;
-    @FXML
-    public Label purchasePrice1;
-    @FXML
-    public Label purchasePrice2;
-    @FXML
-    public Label purchasePrice3;
-    @FXML
-    public Label purchasePrice4;
-
-    @FXML
-    public Label materialsPrice0;
-    @FXML
-    public Label materialsPrice1;
-    @FXML
-    public Label materialsPrice2;
-    @FXML
-    public Label materialsPrice3;
-    @FXML
-    public Label materialsPrice4;
-
-    @FXML
-    public Label laborPrice0;
-    @FXML
-    public Label laborPrice1;
-    @FXML
-    public Label laborPrice2;
-    @FXML
-    public Label laborPrice3;
-    @FXML
-    public Label laborPrice4;
-
-    @FXML
-    public Label spent0;
-    @FXML
-    public Label spent1;
-    @FXML
-    public Label spent2;
-    @FXML
-    public Label spent3;
-    @FXML
-    public Label spent4;
-
-    @FXML
-    public Label estSalePrice0;
-    @FXML
-    public Label estSalePrice1;
-    @FXML
-    public Label estSalePrice2;
-    @FXML
-    public Label estSalePrice3;
-    @FXML
-    public Label estSalePrice4;
-
-    @FXML
-    public Label netProfit0;
-    @FXML
-    public Label netProfit1;
-    @FXML
-    public Label netProfit2;
-    @FXML
-    public Label netProfit3;
-    @FXML
-    public Label netProfit4;
-
-    @FXML
-    public Label totalSpent;
-    @FXML
-    public Label totalProfit;
-
-    @FXML
-    public Button end;
+        // Generate repair options for property
+        for (int i = 0; i < 10; i++) {
+            // Assign property values
+            idx = random.nextInt(Repairs.repairTypes.size());
+            this.propertyDetails.propertyRepairs[i] = Repairs.repairTypes.get(idx);
+            Repairs.repairTypes.remove(idx);
+            }
+    }
 }
